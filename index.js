addcfe85b9d7a893c80acd3e6f9e24c4ed56a6f8
shafanahanna <150221290+shafanahanna@users.noreply.github.com>
2024-10-09 14:57:35 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO)
